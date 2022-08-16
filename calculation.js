6fb41e78ac971b920c2cac26e3f93a99908f5af7
Dainess <paunacoxa@gmail.com>
2022-08-16 19:53:06 -0300

    Erro no commit anterior, .js não havia sido palqueado

--- calculation.js
+++ calculation.js
@@ -14,6 +14,17 @@ for (let i = 0; i < 1000; i++) {
     nums[i] = Math.floor(Math.random() * topo) + 1
 }
 
+var oldRequest = new XMLHttpRequest()
+oldRequest.open('GET', "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BRL-EUR", true)
+
+oldRequest.onload = function () {
+    myData = this.response
+    correctData = JSON.parse(myData)
+    console.log((correctData.EURBRL.bid))
+}
+
+oldRequest.send()
+
 the_max = find_max(nums)
 
 top_num = Number.NEGATIVE_INFINITY;
@@ -22,6 +33,21 @@ nums.forEach((element) => {
         top_num = element;
     }
 });
+
+var request = new XMLHttpRequest()
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
+request.onload = function () {
+    myData = this.response
+    //data = myData.split("},")
+    //console.log(data)
+    correctData = JSON.parse(myData)
+    console.log((correctData[0].title))
+}
+
+request.send()
+
+
 
 /* (function (a) {
     return a + 100;
